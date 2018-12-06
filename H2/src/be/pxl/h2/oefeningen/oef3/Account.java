5b623658353426aef18e92074e9ad250d80fca83
JohnDjupsjobackaPXL <John.Djupsjobacka@student.pxl.be>
2018-12-06 22:09:38 +0100

    Upload oefening 3 done

--- H2/src/be/pxl/h2/oefeningen/oef3/Account.java
+++ H2/src/be/pxl/h2/oefeningen/oef3/Account.java
@@ -12,6 +12,18 @@ public class Account {
         this("none", "unknown", 0, 1.2);
     }
 
+    public Account(String accountNumber) {
+        this.setAccountNumber(accountNumber);
+        System.out.println("Rekeninghouder:");
+        this.setAccountOwner(BankAccountApp.keyboardInput.nextLine());
+        System.out.println("Rekeningsaldo:");
+        this.setAccountBalance(BankAccountApp.keyboardInput.nextDouble());
+        BankAccountApp.keyboardInput.nextLine();
+        System.out.println("Rekening interest:");
+        this.setAccountInterestRate(BankAccountApp.keyboardInput.nextDouble());
+        BankAccountApp.keyboardInput.nextLine();
+    }
+
     public Account(String accountNumber, String accountOwner, double accountBalance, double accountInterestRate) {
         this.setAccountNumber(accountNumber);
         this.setAccountOwner(accountOwner);
@@ -61,31 +73,61 @@ public class Account {
 
     // Methods
     public void deposit(double accountDeposit) {
-        if (accountDeposit > 0) {
-            setAccountBalance(this.accountBalance + accountDeposit);
-        } else {
-            System.out.println("Kan niet een negatief deposito doen!");
+        if (this.accountExists()) {
+            if (accountDeposit > 0) {
+                setAccountBalance(this.accountBalance + accountDeposit);
+            } else {
+                System.out.println("Kan niet een negatief deposito doen!");
+            }
         }
     }
 
     public double withdraw(double accountWithdrawal) {
-        if (accountWithdrawal > 0) {
-            if (this.accountBalance == 0) {
-                System.out.printf("Er is %.2f op de rekening, u kan geen geld opnemen.", this.accountBalance);
+        if (this.accountExists()) {
+            if (accountWithdrawal > 0) {
+                if (this.accountBalance == 0) {
+                    System.out.printf("Er is %.2f op de rekening, u kan geen geld opnemen.", this.accountBalance);
+                    return 0;
+                } else if (accountWithdrawal > this.accountBalance) {
+                    System.out.printf("U kan maximaal %.2f opnemen", this.accountBalance);
+                    accountWithdrawal = this.accountBalance;
+                    setAccountBalance(0);
+                    return accountWithdrawal;
+                } else {
+                    setAccountBalance(this.accountBalance - accountWithdrawal);
+                    return accountWithdrawal;
+                }
+            } else {
+                System.out.println("Kan niet een negatief bedrag afnemen!");
                 return 0;
             }
-            else if (accountWithdrawal > this.accountBalance) {
-                System.out.printf("U kan maximaal %.2f opnemen", this.accountBalance);
-                accountWithdrawal = this.accountBalance;
-                setAccountBalance(0);
-                return accountWithdrawal;
-            } else {
-                setAccountBalance(this.accountBalance - accountWithdrawal);
-                return accountWithdrawal;
-            }
         } else {
-            System.out.println("Kan niet een negatief bedrag afnemen!");
             return 0;
+        }
+    }
+
+    public void accountDividendPayment() {
+        if (this.accountExists()) {
+            this.setAccountBalance(this.accountBalance * (this.accountInterestRate / 100 + 1));
+        }
+    }
+
+    private boolean accountExists() {
+        if (this.accountNumber.equals("none")) {
+            System.out.println("Error, geen rekeningnummer.");
+            return false;
+        } else if (this.accountOwner.equals("unknown")) {
+            System.out.println("Wie is de eigenaar van deze rekening? : ");
+            this.setAccountOwner(BankAccountApp.keyboardInput.nextLine());
+            return true;
+        } else {
+            return true;
+        }
+    }
+
+    public void accountPrintOut() {
+        if (accountExists()) {
+            System.out.printf("Saldo op spaarrekening %s op naam van %s bedraagt %.2f euro.\n", this.getAccountNumber(), this.getAccountOwner(), this.getAccountBalance());
         }
     }
 }
--- H2/src/be/pxl/h2/oefeningen/oef3/BankAccountApp.java
+++ H2/src/be/pxl/h2/oefeningen/oef3/BankAccountApp.java
@@ -1,7 +1,31 @@
 package be.pxl.h2.oefeningen.oef3;
 
-public class BankAccountApp {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class BankAccountApp {
+    // Scanner opzetten voor eventuele keyboard input.
+    public static Scanner keyboardInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Account rekening1 = new Account("001-001-001");
+
+        rekening1.accountPrintOut();
+        rekening1.deposit(50);
+        rekening1.accountPrintOut();
+        rekening1.withdraw(-50);
+
+        Account rekening2 = new Account("001-002-001","Jef Patat", 100.0, 1.1);
+        rekening2.accountPrintOut();
+        rekening2.accountDividendPayment();
+        rekening2.accountPrintOut();
+        System.out.println("Overzetten van balans van rekening 2 naar rekening 1.");
+        rekening1.deposit(rekening2.withdraw(rekening2.getAccountBalance()));
+        rekening1.accountPrintOut();
+        rekening2.accountPrintOut();
+
+
+
+
+        keyboardInput.close();
     }
 }
